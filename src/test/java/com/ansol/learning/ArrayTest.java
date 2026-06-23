62c18efa98a20dbff74d53cfbec219a98e468e6c
Rushikesh Dhakulkar <rushi53520dhakulkar@gmail.com>
2026-06-23 11:36:50 +0530

    assertArrayEquals() Example

---
+++ src/test/java/com/ansol/learning/ArrayTest.java
@@ -0,0 +1,46 @@
+package com.ansol.learning;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ArrayTest
+{
+    @Test
+    void TC1()
+    {
+        int actual[]={10,20,30,40};
+        int expected[]={10,20,30,40};
+        assertArrayEquals(expected,actual);
+
+    }
+    @Test
+    void TC2()
+    {
+        int actual[]={10,20,30,40};
+        int expected[]={10,20,50,40};
+        assertArrayEquals(expected,actual);
+
+    }
+
+    @Test
+    void TC3()
+    {
+        int actual[]={10,20,30,40};
+        int expected[]={10,20,40};
+        assertArrayEquals(expected,actual);
+
+    }
+    @Test
+    void TC4()
+    {
+        int actual[]={10,20,30,40};
+        int expected[]={10,30,20,40};
+        assertArrayEquals(expected,actual);
+
+    }
+
+
+
+
+
+}
--- src/test/java/com/ansol/learning/CalcTest.java
+++ src/test/java/com/ansol/learning/CalcTest.java
@@ -5,14 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
-
+    Calc c=new Calc();
     @Test
-    void divide()
+    void divideTest()
     {
-        Calc c=new Calc();
+
         int actualValue=c.divide(10,5);
         int expectedResult=2;
         assertEquals(expectedResult,actualValue);
 //        System.out.println();
+    }
+
+    @Test
+    void divideTest2()
+    {
+        assertNotEquals(5 , c.divide(10,5),"Test Cases Failed");
     }
 }
